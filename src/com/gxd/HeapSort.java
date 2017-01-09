794b190bd2b06c7b15e61f8ab60edac7e2d729dd
gxdgodgxd <11235.gxd>
2017-01-09 08:28:32 +0800

    1

---
+++ src/com/gxd/HeapSort.java
@@ -0,0 +1,8 @@
+package com.gxd;
+
+/**
+ * Created by gxdgodgxd on 17/1/9.
+ */
+public class HeapSort {
+
+}
--- src/com/gxd/OJ24.java
+++ src/com/gxd/OJ24.java
@@ -1,5 +1,9 @@
 package com.gxd;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gxdgodgxd on 16/9/24.
  */
@@ -22,6 +26,7 @@ public class OJ24 {
     }
 
     public ListNode swapPairsRecursive(ListNode head) {
+        this.getClass().getResourceAsStream("/a");
         if (head == null || head.next == null) return head;
         ListNode p = swapPairs(head.next.next);
         head.next.next = head;
@@ -29,4 +34,26 @@ public class OJ24 {
         head.next = p;
         return res;
     }
+
+    public static void main(String[] args) {
+        InputStream is = OJ24.class.getClass().getResourceAsStream("/abc.txt");
+        if (is != null) {
+            System.out.println("is not null");
+        }else{
+            System.out.println("is is null");
+        }
+        OJ24 oj = new OJ24();
+        oj.swapPairsRecursive(null);
+
+        List<Integer> list = new ArrayList();
+        for (int i = 0; i <100; i++) {
+            list.add(i);
+        }
+        for (int j=list.size()-1;j>=0;j--) {
+            if (list.get(j) % 3 == 0) {
+                list.remove(list.get(j));
+            }
+        }
+    }
+
 }
